88c1056e805fd26279dfc2e222452adcb4cdd6bd
Vaibhav Anurag <anuragvaibhav111@gmail.com>
2024-06-14 01:01:47 +0530

    fixed cookie setting

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.use(session({secret:process.env.SECRET, resave: false,
   secure:true,
   sameSite:'none',
    maxAge: 1000 * 60 * 60 * 24, // 1 day
-   domain: "minimalist-sudoku.onrender.com"
+   domain: "onrender.com"
  }
 }));
 require('./config/passport');
