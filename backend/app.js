4c441b3b4237c630ac9e8ad2d131b00b71c3c8f2
Vaibhav Anurag <anuragvaibhav111@gmail.com>
2024-06-13 23:47:38 +0530

    fixed cookie setting

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use(session({secret:process.env.SECRET, resave: false,
  }),
  cookie: {
   secure:true,
-  sameSite:'none',
+  same_site:'none',
    maxAge: 1000 * 60 * 60 * 24 // 1 day
  }
 }));
