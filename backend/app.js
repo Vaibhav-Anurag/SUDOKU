d8dc2f84907cf7eb1a13ebd53c36b83220b3390c
Vaibhav Anurag <anuragvaibhav111@gmail.com>
2024-06-14 01:37:55 +0530

    fixed cookie setting

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,8 @@ app.use(session({secret:process.env.SECRET, resave: false,
  cookie: {
    maxAge: 1000 * 60 * 60 * 24, // 1 day
    sameSite:'none',
-   secure:true
+   secure:true,
+   httpOnly:true
  }
 }));
 require('./config/passport');
