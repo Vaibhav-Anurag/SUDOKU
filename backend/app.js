2899c2f5c6cb0abf0b353dffc5eb6ceaba25de12
Vaibhav Anurag <anuragvaibhav111@gmail.com>
2024-06-14 10:59:50 +0530

    pki validation

--- backend/app.js
+++ backend/app.js
@@ -29,8 +29,7 @@ async function main() {
 var app = express();
 app.enable('trust-proxy');
 
-app.use(express.static(path.join(__dirname, '/.well-known/pki-validaion')));
-
+app.use(express.static(path.join(__dirname, '/')));
 // Define a route to serve the verification file
 app.get('/.well-known/pki-validation/36D0DC5ECCC6F69C4F7D50FDF95B9F84.txt', (req, res) => {
   // Assuming the file is located in the root directory
