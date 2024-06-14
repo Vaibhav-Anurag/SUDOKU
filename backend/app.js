27434c466f212baa972a094249a9cfd7478355a4
Vaibhav Anurag <anuragvaibhav111@gmail.com>
2024-06-14 11:10:10 +0530

    pki validation

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, '/')));
 app.get('/.well-known/pki-validation/36D0DC5ECCC6F69C4F7D50FDF95B9F84.txt', (req, res) => {
   // Assuming the file is located in the root directory
   console.log('get ssl file recieved');
-  const filePath = path.join(__dirname, '.well-known/pki-validation/36D0DC5ECCC6F69C4F7D50FDF95B9F84.txt');
+  const filePath = path.join(__dirname, '36D0DC5ECCC6F69C4F7D50FDF95B9F84.txt');
   
   // Send the file back to the client
   res.sendFile(filePath);
